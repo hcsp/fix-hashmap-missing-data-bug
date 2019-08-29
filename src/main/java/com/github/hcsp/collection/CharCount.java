7b28008e673574d281ed021434580b37b930944d
胡凯波 <1069977596@qq.com>
2019-08-29 10:30:09 +0800

    运用debug,找到是由于theirChar.retainAll(myChars)方法后,导致丢失了数据。

--- src/main/java/com/github/hcsp/collection/CharCount.java
+++ src/main/java/com/github/hcsp/collection/CharCount.java
@@ -1,6 +1,9 @@
 package com.github.hcsp.collection;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class CharCount {
     /**
