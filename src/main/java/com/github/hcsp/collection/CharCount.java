fe8706eb6638fb0325507b3a5203776fd129a3e4
Kelvin Chen <wheelchen97@gmail.com>
2019-11-18 22:44:08 +0800

    fix-hashmap-missing-data-bug (#54)
    fix-hashmap-missing-data-bug (#54)

--- src/main/java/com/github/hcsp/collection/CharCount.java
+++ src/main/java/com/github/hcsp/collection/CharCount.java
@@ -44,8 +44,16 @@ public class CharCount {
     public int howManyCharsInCommon(CharCount anotherCharCount) {
         Set<Character> myChars = chars();
         Set<Character> theirChars = anotherCharCount.chars();
+        // copy set
+//        HashSet<Character> ret = new HashSet<>(theirChars);
+//        ret.retainAll(myChars);
 
-        theirChars.retainAll(myChars);
-        return theirChars.size();
+        int cnt = 0;
+        for (Character ch: theirChars) {
+            if (myChars.contains(ch)) {
+                cnt++;
+            }
+        }
+        return cnt;
     }
 }
