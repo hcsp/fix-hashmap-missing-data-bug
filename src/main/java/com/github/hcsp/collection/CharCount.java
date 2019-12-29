88c7426cbec256076dcd5b4c3480ffde5ddd171f
Xi Pu <30915281+XiPu99@users.noreply.github.com>
2019-12-29 14:33:35 -0800

    fix bug in CharCount (#64)
    fix bug in CharCount (#64)

--- src/main/java/com/github/hcsp/collection/CharCount.java
+++ src/main/java/com/github/hcsp/collection/CharCount.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.collection;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -37,7 +38,7 @@ public class CharCount {
      * @return 包含的所有字符集合
      */
     public Set<Character> chars() {
-        return charCount.keySet();
+        return new HashSet<>(charCount.keySet());
     }
 
     // 我和另外一个CharCount有多少个公共字符？ 例如，aabbcc和abcdef有3个公共字符: a/b/c，因此返回3
