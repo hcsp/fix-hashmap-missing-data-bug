f02a04c5898dc84370a59cbc1fd83d6c5c5e04b3
silvia-YQY <545067444@qq.com>
2020-02-09 00:10:23 +0800

    checkStyle:fix

--- src/main/java/com/github/hcsp/collection/CharCount.java
+++ src/main/java/com/github/hcsp/collection/CharCount.java
@@ -44,7 +44,7 @@ public class CharCount {
     // 我和另外一个CharCount有多少个公共字符？ 例如，aabbcc和abcdef有3个公共字符: a/b/c，因此返回3
     public int howManyCharsInCommon(CharCount anotherCharCount) {
         Set<Character> myChars = chars();
-        Set<Character> theirChars = new HashSet<>(anotherCharCount.chars()) ;
+        Set<Character> theirChars = new HashSet<>(anotherCharCount.chars());
 
         theirChars.retainAll(myChars);
         return theirChars.size();
@@ -57,3 +57,4 @@ public class CharCount {
         System.out.println(charCount2.count('d'));
     }
 }
+
