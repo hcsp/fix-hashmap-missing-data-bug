4a0f5f0f680f6e55b1e460a394ebb0501d57f7e2
pathjh <1639925487@qq.com>
2020-03-06 22:19:18 +0800

    集合知识运用 (#96)
    集合知识运用 (#96)

--- src/main/java/com/github/hcsp/collection/CharCount.java
+++ src/main/java/com/github/hcsp/collection/CharCount.java
@@ -14,6 +14,11 @@ public class CharCount {
      * c -> 1
      * </pre>
      */
+    /**
+     * hashmap不能存储相同的键，使用put方法时，若指定的键(key)在集合中存在，
+     * 则返回值为集合中键对应的值（该值为替换前的值），并把指定键所对应的值，替换成指定的新值。
+     */
+
     private final Map<Character, Integer> charCount = new HashMap<>();
 
     public CharCount(String s) {
@@ -30,6 +35,11 @@ public class CharCount {
     public int count(char ch) {
         return charCount.getOrDefault(ch, 0);
     }
+    /**
+     * Map.getOrDefault(Object key, V defaultValue)方法的作用是：
+     *   当Map集合中有这个key时，就使用这个key对应的值；
+     *   如果没有就使用默认值defaultValue。
+     */
 
     /**
      * 我到底包含哪些字符？
@@ -37,7 +47,8 @@ public class CharCount {
      * @return 包含的所有字符集合
      */
     public Set<Character> chars() {
-        return charCount.keySet();
+        //return charCount.keySet();
+        return new HashMap<>(charCount).keySet();
     }
 
     // 我和另外一个CharCount有多少个公共字符？ 例如，aabbcc和abcdef有3个公共字符: a/b/c，因此返回3
