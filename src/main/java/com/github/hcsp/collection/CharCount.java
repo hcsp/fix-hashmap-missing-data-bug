5108f00f6a4071960f7ee504bf1634130439f0f8
YUCHEN LIU <181484908@QQ.COM>
2020-05-11 10:27:06 +0800

    work: hashmap bug (#119)
    work: hashmap bug (#119)

--- src/main/java/com/github/hcsp/collection/CharCount.java
+++ src/main/java/com/github/hcsp/collection/CharCount.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.collection;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -44,8 +45,8 @@ public class CharCount {
     public int howManyCharsInCommon(CharCount anotherCharCount) {
         Set<Character> myChars = chars();
         Set<Character> theirChars = anotherCharCount.chars();
-
-        theirChars.retainAll(myChars);
-        return theirChars.size();
+        Set<Character> retainResult = new HashSet<>(theirChars);
+        retainResult.retainAll(myChars);
+        return retainResult.size();
     }
 }
