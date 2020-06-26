935a87a56ce3ce210edc0369c6a3669b0a652f15
summer <summer_xue.zz@foxmail.com>
2020-06-26 10:34:26 +0800

    retainAll方法之后，会改变theirChars的内容

--- src/main/java/com/github/hcsp/collection/CharCount.java
+++ src/main/java/com/github/hcsp/collection/CharCount.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.collection;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -44,8 +45,14 @@ public class CharCount {
     public int howManyCharsInCommon(CharCount anotherCharCount) {
         Set<Character> myChars = chars();
         Set<Character> theirChars = anotherCharCount.chars();
-
-        theirChars.retainAll(myChars);
-        return theirChars.size();
+        Set<Character> commonElement = new HashSet<>();
+        for (Character theirCharacter : theirChars) {
+            for (Character myCharacter : myChars) {
+                if (theirCharacter.equals(myCharacter)) {
+                    commonElement.add(theirCharacter);
+                }
+            }
+        }
+        return commonElement.size();
     }
 }
