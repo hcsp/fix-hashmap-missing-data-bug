7f422d8add89d4728951a1426ebda0ad95ff4762
MorganXie <xx529303860@outlook.com>
2021-01-17 17:25:59 +0800

    keySet()会对原数据修改。 (#158)
    keySet()会对原数据修改。 (#158)

--- src/main/java/com/github/hcsp/collection/CharCount.java
+++ src/main/java/com/github/hcsp/collection/CharCount.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.collection;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -37,14 +38,13 @@ public class CharCount {
      * @return 包含的所有字符集合
      */
     public Set<Character> chars() {
-        return charCount.keySet();
+        return new HashSet<>(charCount.keySet());
     }
 
     // 我和另外一个CharCount有多少个公共字符？ 例如，aabbcc和abcdef有3个公共字符: a/b/c，因此返回3
     public int howManyCharsInCommon(CharCount anotherCharCount) {
         Set<Character> myChars = chars();
         Set<Character> theirChars = anotherCharCount.chars();
-
         theirChars.retainAll(myChars);
         return theirChars.size();
     }
