569360ee201b8b12cd3cb1118b8235e095e14fe6
yangdepp <yangdepp@163.com>
2021-04-01 23:06:50 +0800

    done (#163)
    done (#163)

--- src/main/java/com/github/hcsp/collection/CharCount.java
+++ src/main/java/com/github/hcsp/collection/CharCount.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.collection;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -45,7 +46,9 @@ public class CharCount {
         Set<Character> myChars = chars();
         Set<Character> theirChars = anotherCharCount.chars();
 
-        theirChars.retainAll(myChars);
-        return theirChars.size();
+//        theirChars.retainAll(myChars);
+        Set<Character> character = new HashSet<>(theirChars);
+        character.retainAll(myChars);
+        return character.size();
     }
 }
